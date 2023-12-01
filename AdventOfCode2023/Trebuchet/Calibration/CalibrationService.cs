c413b2537274d4a1d1ee2ff022760ca9847d1144
Robin de Gier <de.gier.robin@gmail.com>
2023-12-01 16:29:34 +0100

    Finish assignment 1

--- AdventOfCode2023/Trebuchet/Calibration/CalibrationService.cs
+++ AdventOfCode2023/Trebuchet/Calibration/CalibrationService.cs
@@ -1,6 +1,32 @@
+using AdventOfCode2023.Extensions;
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2023.Trebuchet.Calibration;
 
-public class CalibrationService : ICalibrationService
+public partial class CalibrationService : ICalibrationService
 {
-    public CalibrationResult Calibrate(string input) => new(new List<int>(), 0);
+    public CalibrationResult Calibrate(string input)
+    {
+        var calibrationValues = input
+            .SplitByLineBreaks()
+            .Select(RetrieveCalibrationValue)
+            .ToList();
+
+        var total = calibrationValues.Sum();
+
+        return new CalibrationResult(calibrationValues, total);
+    }
+
+    private int RetrieveCalibrationValue(string calibrationLine)
+    {
+        var firstNumber = int.Parse(FirstNumberRegex().Match(calibrationLine).Groups["firstNumber"].Value);
+        var lastNumber = int.Parse(LastNumberRegex().Match(calibrationLine).Groups["lastNumber"].Value);
+        return firstNumber * 10 + lastNumber;
+    }
+
+    [GeneratedRegex(@"(?<firstNumber>\d)")]
+    private static partial Regex FirstNumberRegex();
+
+    [GeneratedRegex(@".*(?<lastNumber>\d)\w*$")]
+    private static partial Regex LastNumberRegex();
 }
